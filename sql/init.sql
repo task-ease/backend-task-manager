16d8079550b8965ca121c1b088ad73890a0219eb
DenisSTK <denisdrawler@gmail.com>
2025-05-30 19:21:54 +0300

    added tasks in init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -2,23 +2,40 @@ CREATE TABLE IF NOT EXISTS users (
                                      id uuid PRIMARY KEY,
                                      username VARCHAR(50) NOT NULL,
                                      email VARCHAR(100) NOT NULL UNIQUE,
-                                     password_hash TEXT NOT NULL,
+                                     password_hash text NOT NULL,
                                      user_icon_url TEXT,
                                      role VARCHAR(20) DEFAULT 'user',
-                                     created_at TIMESTAMP DEFAULT NOW()
+                                     created_at timestamptz DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS workspaces (
-                                          id SERIAL PRIMARY KEY,
+                                          id uuid PRIMARY KEY,
                                           name VARCHAR(20) NOT NULL,
-                                          created_at TIMESTAMP DEFAULT NOW()
+                                          created_at timestamptz DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS user_workspaces (
-                                               user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-                                               workspace_id INTEGER REFERENCES workspaces(id) on delete CASCADE,
+                                               user_id uuid REFERENCES users(id) ON DELETE CASCADE,
+                                               workspace_id uuid REFERENCES workspaces(id) ON DELETE CASCADE,
                                                role VARCHAR(20) DEFAULT 'member',
-                                               joined_at TIMESTAMP DEFAULT NOW(),
+                                               joined_at timestamptz DEFAULT NOW(),
                                                PRIMARY KEY (user_id, workspace_id)
+);
 
+CREATE TABLE IF NOT EXISTS tasks (
+                                     id uuid PRIMARY KEY,
+                                     author_id uuid REFERENCES users(id) ON DELETE CASCADE,
+                                     created_at TIMESTAMPTZ DEFAULT NOW(),
+                                     title VARCHAR(20) NOT NULL,
+                                     description TEXT,
+                                     is_finished BOOLEAN DEFAULT FALSE,
+                                     due_date TIMESTAMPTZ,
+                                     priority INTEGER,
+                                     updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS tasks_assignment (
+                                                task_id uuid REFERENCES tasks(id) ON DELETE CASCADE,
+                                                user_id uuid REFERENCES users(id) ON DELETE CASCADE,
+                                                PRIMARY KEY (task_id, user_id)
 );
